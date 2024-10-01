424aadff69b1654a652819b179dc0fb1f47e963e
MedTrebor <medovarski.robert@gmail.com>
2024-10-01 10:06:06 +0200

    Finished working version of CSV app

--- src/main/java/rs/company/CSVApp.java
+++ src/main/java/rs/company/CSVApp.java
@@ -5,6 +5,7 @@ import org.apache.camel.impl.DefaultCamelContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rs.company.datasource.DbDataSource;
+import rs.company.route.csv.CSVRoute;
 
 public class CSVApp {
 
@@ -13,8 +14,10 @@ public class CSVApp {
     public static void main(String[] args) {
 
         try (CamelContext context = new DefaultCamelContext()) {
-            context.start();
             context.getRegistry().bind("db", DbDataSource.get());
+            context.addRoutes(new CSVRoute());
+            context.start();
+            context.createProducerTemplate().sendBody("direct:toCSVFile", null);
             context.stop();
         } catch (Exception e) {
             LOG.error(e.getMessage());
