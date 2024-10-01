4f58ef05bf5099bb873948c9ba27afa65d55f9e5
MedTrebor <medovarski.robert@gmail.com>
2024-10-01 09:57:28 +0200

    Created CSV route

---
+++ src/main/java/rs/company/route/csv/CSVRoute.java
@@ -0,0 +1,18 @@
+package rs.company.route.csv;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.BindyType;
+import rs.company.candidate.Candidate;
+
+public class CSVRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("direct:toCSVFile")
+            .setBody(constant("select * from candidates"))
+            .to("jdbc:db?outputClass=rs.company.candidate.Candidate")
+            .marshal()
+            .bindy(BindyType.Csv, Candidate.class)
+            .to("file:src/data?fileName=candidates.csv");
+    }
+}
